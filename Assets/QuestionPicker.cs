3aec8b22d68d9d728b631fd984e256471d45b4ac
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-08-29 14:46:33 +0200

    Split Questions (initialization, save&load) from QuestionPicker

--- Assets/QuestionPicker.cs
+++ Assets/QuestionPicker.cs
@@ -3,62 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class QuestionPicker : MonoBehaviour {
-	private Question[] questions;
-	private const string prefsKey = "questions";
-	[SerializeField] private int maxNum = 3;
+	private Questions questions;
 	[SerializeField] QuestionDisplay display;
 	private Question curQuestion;
 
 	// Use this for initialization
 	void Start () {
-		InitQuestions ();
+		questions = new Questions();
 		curQuestion = PickQuestion ();
 		display.DisplayQuestion (curQuestion.GetQuestionString ());
 	}
 
-	void InitQuestions() {		
-		CreateQuestions ();
-		Load ();
-		for (int i = 0; i < questions.Length; ++i) {
-			Debug.Log (questions [i]);
-		}
-		Save ();
-	}
-
-	void CreateQuestions() {
-		questions = new Question[maxNum * (maxNum+1) /2];
-		int idx = 0;
-		for (int a = 1; a <= maxNum; ++a) {
-			for (int b = 1; b <= a; ++b) {
-				questions [idx] = new Question (a, b);
-				++idx;
-			}
-		}
-	}
-	
 	Question PickQuestion() {
-		return questions[Random.Range(0, questions.Length)];
+		Question[] qArray = questions.GetArray ();
+		return qArray[Random.Range(0, qArray.Length)];
 	}
 
-	public void Reset() {
-		PlayerPrefs.DeleteKey (prefsKey);
-	}
-
-	void Load() {
-		if (PlayerPrefs.HasKey(prefsKey)) {
-			UnityEngine.Assertions.Assert.AreEqual (PlayerPrefs.GetInt (prefsKey + ":ArrayLen"), questions.Length);
-		}
-		for (int i = 0; i < questions.Length; ++i) {
-			questions [i].Load (prefsKey + ":" + i.ToString ());
-		}
-	}
-
-	void Save() {
-		PlayerPrefs.SetInt(prefsKey + ":ArrayLen", questions.Length);
-		for(int i = 0; i < questions.Length; ++i) {
-			questions [i].Save (prefsKey + ":" + i.ToString());
-		}
-	}
 
 
 }
---
+++ Assets/Questions.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Questions {
+	private Question[] questions;
+	private const string prefsKey = "questions";
+	[SerializeField] private int maxNum = 3;
+
+	// Use this for initialization
+	public Questions() {
+		CreateQuestions ();
+		Load ();
+		for (int i = 0; i < questions.Length; ++i) {
+			Debug.Log (questions [i]);
+		}
+		Save ();
+	}
+
+	public Question[] GetArray() {
+		return questions;
+	}
+
+	void CreateQuestions() {
+		questions = new Question[maxNum * (maxNum+1) /2];
+		int idx = 0;
+		for (int a = 1; a <= maxNum; ++a) {
+			for (int b = 1; b <= a; ++b) {
+				questions [idx] = new Question (a, b);
+				++idx;
+			}
+		}
+	}
+
+//	public void Reset() {
+//		PlayerPrefs.DeleteKey (prefsKey);
+//	}
+
+	void Load() {
+		if (PlayerPrefs.HasKey(prefsKey)) {
+			UnityEngine.Assertions.Assert.AreEqual (PlayerPrefs.GetInt (prefsKey + ":ArrayLen"), questions.Length);
+		}
+		for (int i = 0; i < questions.Length; ++i) {
+			questions [i].Load (prefsKey + ":" + i.ToString ());
+		}
+	}
+
+	void Save() {
+		PlayerPrefs.SetInt(prefsKey + ":ArrayLen", questions.Length);
+		for(int i = 0; i < questions.Length; ++i) {
+			questions [i].Save (prefsKey + ":" + i.ToString());
+		}
+	}
+
+}
