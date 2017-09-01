20f1d5c1c6b1a624fb48dc91595d30f44f1b7a7d
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-09-01 13:59:00 +0200

    Add score.

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour, OnWrongAnswer, OnCorrectAnswer {
+	[SerializeField] UnityEngine.UI.Text text = null;
+	[SerializeField] float delay = 0.5f;
+	[SerializeField] float duration = 1.0f;
+	[SerializeField] int maxMultiplier = 4;
+	int score;
+	int multiplier;
+
+	void Start() {
+		text.text = "0";
+	}
+
+	public void OnWrongAnswer() {
+		multiplier = 0;
+	}
+
+	public void OnCorrectAnswer(Question question) {
+		if (multiplier < maxMultiplier) {
+			++multiplier;
+		}
+		score += multiplier * question.a * question.b;
+		UpdateScoreDisplay (score);
+	}
+
+	void UpdateScoreDisplay(int newScore) {
+		int oldScore = 0;
+		StopAllCoroutines ();
+		if (int.TryParse(text.text, out oldScore)) {
+			StartCoroutine (CountTextUp (oldScore, newScore));
+		} else {
+			text.text = newScore.ToString();
+		}
+	}
+
+	IEnumerator CountTextUp(int oldScore, int newScore) {
+		yield return new WaitForSeconds (delay);
+		float numbersPerSec = (newScore - oldScore) / duration;
+		for (int i = oldScore; i <= newScore; i += Mathf.CeilToInt(Time.deltaTime * numbersPerSec)) {
+			text.text = i.ToString();
+			yield return null;
+		}
+	}
+}
