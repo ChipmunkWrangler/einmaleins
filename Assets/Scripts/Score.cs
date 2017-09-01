f6fe22cd920e2ba03a93e35fe63c0949a3cd1dbf
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-09-01 14:42:26 +0200

    Visualize multipliers with stars.

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -6,8 +6,9 @@ public class Score : MonoBehaviour, OnWrongAnswer, OnCorrectAnswer {
 	[SerializeField] UnityEngine.UI.Text scoreText = null;
 	[SerializeField] UnityEngine.UI.Text highScoreText = null;
 	[SerializeField] float delay = 0.5f;
-	[SerializeField] float duration = 1.0f;
-	[SerializeField] int maxMultiplier = 4;
+	[SerializeField] float scoreCountUpDuration = 2.5f;
+	[SerializeField] UnityEngine.UI.Image[] multiplierIcons;
+	[SerializeField] float multiplierFadeDuration;
 	int score;
 	int multiplier;
 	int highScore;
@@ -19,10 +20,19 @@ public class Score : MonoBehaviour, OnWrongAnswer, OnCorrectAnswer {
 		scoreText.text = "0";
 		highScoreText.text = "0";
 		SetHighScore (PlayerPrefs.GetInt (prefsKey));
+		foreach (var multiplierIcon in multiplierIcons) {
+			multiplierIcon.CrossFadeAlpha (0f, 0, false);
+//			Color c = multiplierIcon.color;
+//			c.a = 0;
+//			multiplierIcon.color = c;
+		}
 	}
 
 	public void OnWrongAnswer() {
 		multiplier = 0;
+		foreach (var multiplierIcon in multiplierIcons) {
+			multiplierIcon.CrossFadeAlpha (0.0f, multiplierFadeDuration, false);
+		}
 	}
 
 	public void OnCorrectAnswer(Question question) {
@@ -31,9 +41,11 @@ public class Score : MonoBehaviour, OnWrongAnswer, OnCorrectAnswer {
 	}
 
 	void IncrementMultiplier() {
-		if (multiplier < maxMultiplier) {
+		if (multiplier < multiplierIcons.Length) {
 			++multiplier;
 		}
+		print ("fading star " + multiplier + " " + multiplierIcons [multiplier - 1]);
+		multiplierIcons [multiplier - 1].CrossFadeAlpha (1.0f, multiplierFadeDuration, false);
 	}
 
 	void IncreaseScoreBy( int amt) {
@@ -70,7 +82,7 @@ public class Score : MonoBehaviour, OnWrongAnswer, OnCorrectAnswer {
 	IEnumerator CountTextUp(UnityEngine.UI.Text text, int oldScore, int newScore) {
 		print ("CountTextUp" + newScore);
 		yield return new WaitForSeconds (delay);
-		float numbersPerSec = (newScore - oldScore) / duration;
+		float numbersPerSec = (newScore - oldScore) / scoreCountUpDuration;
 		for (int i = oldScore; i <= newScore; i += Mathf.CeilToInt(Time.deltaTime * numbersPerSec)) {
 			text.text = i.ToString();
 			print ("counting " + i + " of " + newScore);
