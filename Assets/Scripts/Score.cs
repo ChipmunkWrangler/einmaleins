4dbc22ffa687684f153e1026a6a64c7410c40796
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-09-01 14:19:55 +0200

    Add high score.

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Score : MonoBehaviour, OnWrongAnswer, OnCorrectAnswer {
-	[SerializeField] UnityEngine.UI.Text text = null;
+	[SerializeField] UnityEngine.UI.Text scoreText = null;
+	[SerializeField] UnityEngine.UI.Text highScoreText = null;
 	[SerializeField] float delay = 0.5f;
 	[SerializeField] float duration = 1.0f;
 	[SerializeField] int maxMultiplier = 4;
 	int score;
 	int multiplier;
+	int highScore;
+	const string prefsKey = "highScore";
+	Coroutine highScoreCoroutine;
+	Coroutine scoreCoroutine;
 
 	void Start() {
-		text.text = "0";
+		scoreText.text = "0";
+		highScoreText.text = "0";
+		SetHighScore (PlayerPrefs.GetInt (prefsKey));
 	}
 
 	public void OnWrongAnswer() {
@@ -19,28 +26,54 @@ public class Score : MonoBehaviour, OnWrongAnswer, OnCorrectAnswer {
 	}
 
 	public void OnCorrectAnswer(Question question) {
+		IncrementMultiplier ();
+		IncreaseScoreBy (multiplier * question.a * question.b);
+	}
+
+	void IncrementMultiplier() {
 		if (multiplier < maxMultiplier) {
 			++multiplier;
 		}
-		score += multiplier * question.a * question.b;
-		UpdateScoreDisplay (score);
 	}
 
-	void UpdateScoreDisplay(int newScore) {
-		int oldScore = 0;
-		StopAllCoroutines ();
-		if (int.TryParse(text.text, out oldScore)) {
-			StartCoroutine (CountTextUp (oldScore, newScore));
-		} else {
-			text.text = newScore.ToString();
+	void IncreaseScoreBy( int amt) {
+		score += amt;
+		if (scoreCoroutine != null) {
+			StopCoroutine (scoreCoroutine);
+		}
+		scoreCoroutine = UpdateScoreDisplay (scoreText, score);
+		if (score > highScore) {
+			SetHighScore (score);
 		}
 	}
 
-	IEnumerator CountTextUp(int oldScore, int newScore) {
+	void SetHighScore(int newHighScore) {
+		print ("newHighScore" + newHighScore);
+		highScore = newHighScore;
+		PlayerPrefs.SetInt (prefsKey, highScore);
+		if (highScoreCoroutine != null) {
+			StopCoroutine (highScoreCoroutine);
+		}
+		highScoreCoroutine = UpdateScoreDisplay (highScoreText, highScore);
+	}
+
+	Coroutine UpdateScoreDisplay(UnityEngine.UI.Text text, int newScore) {
+		int oldScore = 0;
+		if (int.TryParse(text.text, out oldScore)) {
+			return StartCoroutine (CountTextUp (text, oldScore, newScore));
+		} else {
+			text.text = newScore.ToString();
+		}
+		return null;
+	}
+
+	IEnumerator CountTextUp(UnityEngine.UI.Text text, int oldScore, int newScore) {
+		print ("CountTextUp" + newScore);
 		yield return new WaitForSeconds (delay);
 		float numbersPerSec = (newScore - oldScore) / duration;
 		for (int i = oldScore; i <= newScore; i += Mathf.CeilToInt(Time.deltaTime * numbersPerSec)) {
 			text.text = i.ToString();
+			print ("counting " + i + " of " + newScore);
 			yield return null;
 		}
 	}
