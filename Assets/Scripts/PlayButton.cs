7a213a6ba5be2e55f5488e3c1c5ccef2bc86ca62
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-09-02 08:38:32 +0200

    Added new player selection screen (UI, not yet hooked up)

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour {
+	UnityEngine.UI.Button button;
+
+	void Start() {
+		button = GetComponent<UnityEngine.UI.Button> ();
+		button.interactable = false;
+	}
+
+	public void OnPlayerNameChanged(string name) {
+		button.interactable = name.Length > 0;
+	}
+}
+
