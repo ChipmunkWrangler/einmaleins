7357887130d4526c53c2d1280534380769554cf2
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-09-04 10:37:41 +0200

    Moved PositionFX into scripts subdir

--- Assets/Scripts/PositionFx.cs
+++ Assets/Scripts/PositionFx.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PositionFx : MonoBehaviour {
+	[SerializeField] Transform referencePoint;
 
+	bool isSet;
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (!isSet) { 
+			isSet = true;
+			print (transform.position);
+			gameObject.transform.position = Camera.main.WorldToScreenPoint(transform.position);
+		}
 	}
 }
