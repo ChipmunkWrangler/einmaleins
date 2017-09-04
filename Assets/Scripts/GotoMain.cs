09e3342e8b2a0782cde86d1f561ad95ec4cc63e0
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-09-04 21:15:03 +0200

    Added stats scene dummy

---
+++ Assets/Scripts/GotoMain.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GotoMain : MonoBehaviour {
+
+	public void LoadMainScene() {
+		PlayerPrefs.Save ();
+		UnityEngine.SceneManagement.SceneManager.LoadSceneAsync ("main");
+	}
+}
---
+++ Assets/Scripts/GotoStats.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GotoStats : MonoBehaviour {
+
+	public void LoadStatsScene() {
+		PlayerPrefs.Save ();
+		UnityEngine.SceneManagement.SceneManager.LoadSceneAsync ("stats");
+	}
+}
