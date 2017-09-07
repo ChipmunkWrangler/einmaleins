364204c23df0ca8ebe692248659c706053bdadbe
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-09-07 20:53:34 +0200

    Added countdown and rocket flames.

---
+++ Assets/Scripts/KickoffLaunch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KickoffLaunch : MonoBehaviour {
+	[SerializeField] QuestionPicker questionPicker = null;
+	[SerializeField] float delay = 1.0f;
+	[SerializeField] int countdownTime = 3;
+	[SerializeField] UnityEngine.UI.Text countdownText = null;
+	[SerializeField] GameObject[] uiElementsToActivate;
+
+	void Start () {
+		StartCoroutine (Kickoff ());
+	}
+
+	IEnumerator Kickoff () {
+		countdownText.text = "";
+		countdownText.gameObject.SetActive (true);
+		yield return new WaitForSeconds (delay);
+		for (int i = countdownTime; i > 0; --i) {
+			countdownText.text = i.ToString ();
+			yield return new WaitForSeconds (1.0f);
+		}
+		countdownText.text = "";
+		countdownText.gameObject.SetActive (false);
+		foreach (var element in uiElementsToActivate) {
+			element.SetActive (true);
+		}
+		yield return null;
+		questionPicker.NextQuestion ();
+	}
+}
