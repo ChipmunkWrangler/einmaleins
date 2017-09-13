564d0a6584b5de06ff62573433864f3e051e74cb
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-09-13 09:28:14 +0200

    Highlight stats button after a new fact has been mastered. Highlight yellow instead of white.

--- Assets/Scripts/RocketPartCounter.cs
+++ Assets/Scripts/RocketPartCounter.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RocketPartCounter : MonoBehaviour, OnCorrectAnswer, OnQuestionChanged {
-	[SerializeField] UnityEngine.UI.Text numText = null;
-	[SerializeField] UnityEngine.UI.Text labelText = null;
+	[SerializeField] Text numText = null;
+	[SerializeField] Image[] imagesToHighlight;
+	[SerializeField] Text[] textsToHighlight;
 	[SerializeField] Color highlightColour;
 	[SerializeField] float highlightFadeTime;
 	int rocketParts;
 	const string prefsKey = "rocketParts";
-	Color baseColor;
+	Color[] baseColor;
 
 	void Awake() {
-		baseColor = numText.color;
+		baseColor = new Color[textsToHighlight.Length];
+		int i = 0;
+		foreach (Text text in textsToHighlight) {
+			baseColor [i++] = text.color;
+		}
 	}
 
 	void Start () {
@@ -21,28 +27,44 @@ public class RocketPartCounter : MonoBehaviour, OnCorrectAnswer, OnQuestionChang
 	}
 
 	public void OnQuestionChanged(Question question) {
-		StartCoroutine (FadeText (baseColor, highlightFadeTime, numText));
-		StartCoroutine (FadeText (baseColor, highlightFadeTime, labelText));
+		int i = 0;
+		foreach (Text text in textsToHighlight) {
+			StartCoroutine (FadeText (baseColor[i++], highlightFadeTime, text));
+		}
 	}
 
 	public void OnCorrectAnswer(Question question, bool isNewlyMastered) {
 		if (isNewlyMastered) {
 			++rocketParts;
-			StartCoroutine (FadeText (highlightColour, highlightFadeTime, numText));
-			StartCoroutine (FadeText (highlightColour, highlightFadeTime, labelText));
+			foreach (Image image in imagesToHighlight) {
+				StartCoroutine (FadeImage (highlightColour, highlightFadeTime, image));
+			}
+			foreach (Text text in textsToHighlight) {
+				StartCoroutine (FadeText (highlightColour, highlightFadeTime, text));
+			}
 			MDPrefs.SetInt (prefsKey, rocketParts);
 			UpdateText ();
 		}
 	}
 
-	IEnumerator FadeText(Color end, float fadeTime, UnityEngine.UI.Text text) {
-		Color start = text.color;
+	IEnumerator FadeText(Color end, float fadeTime, Text text) {
+		Color startColor = text.color;
 		float startTime = Time.time;
 		float t = 0;
 		while (t <= 1.0f) {
-			text.color = Color.Lerp (start, end, t);
+			text.color = Color.Lerp (startColor, end, t);
 			t = (Time.time - startTime) / fadeTime;
-			Debug.Log (text.color + " " + t);
+			yield return null;
+		}
+	}
+
+	IEnumerator FadeImage(Color end, float fadeTime, Image image) {
+		Color startColor = image.color;
+		float startTime = Time.time;
+		float t = 0;
+		while (t <= 1.0f) {
+			image.color = Color.Lerp (startColor, end, t);
+			t = (Time.time - startTime) / fadeTime;
 			yield return null;
 		}
 	}
