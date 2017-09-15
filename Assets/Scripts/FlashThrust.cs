6d9d9a8f3695a0007c88834dd275b303caee0c9d
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-09-15 18:41:12 +0200

    Made game less punishing: capped falling speed, and acceleration can never start from a negative, so you can't dig yourself into a hole.

--- Assets/Scripts/FlashThrust.cs
+++ Assets/Scripts/FlashThrust.cs
@@ -10,15 +10,19 @@ public class FlashThrust : MonoBehaviour, OnCorrectAnswer, OnQuestionChanged {
 	[SerializeField] ScrollBackground background = null;
 	[SerializeField] float[] maxAttainableHeights = null;
 	[SerializeField] float targetAnswerTime = 5.0f; // If a player answers all questions correctly, each in targetAnswerTime, she reaches maxAttainableHeight -- remember to include celebration time!
+	[SerializeField] float minSpeedFactor = 0.25f;
 	[SerializeField] KickoffLaunch launch = null;
 	[SerializeField] FlashQuestions flashQuestions = null;
 	[SerializeField] Celebrate celebrate;
 
+//	float startTime;
+//	float prevTime;
+	float minSpeed;
 	public float speed { get; private set; } // km per second
 	public float accelerationOnCorrect { get; private set; } // total speed increase per correct answer.
 	public float height { get; private set; } // km
 
-	float gravity = 9.8f;
+	float gravity = 50f;
 	float recordHeight;
 	float apogee;
 	bool isRunning;
@@ -35,9 +39,9 @@ public class FlashThrust : MonoBehaviour, OnCorrectAnswer, OnQuestionChanged {
 		apogeeText.text = "0";
 		recordHeight = MDPrefs.GetFloat (prefsKey, 0);
 		recordHeightText.text = recordHeight.ToString (numFormat) + unit;
-//		accelerationOnCorrect = CalcAcceleration(maxAttainableHeight, FlashQuestions.ASK_LIST_LENGTH);
 		UnityEngine.Assertions.Assert.AreEqual(RocketParts.GetNumUpgrades() + 1, maxAttainableHeights.Length);
 		CalcParams(maxAttainableHeights[RocketParts.GetUpgradeLevel()], FlashQuestions.ASK_LIST_LENGTH);
+//		accelerationOnCorrect = CalcAcceleration(maxAttainableHeights[RocketParts.GetUpgradeLevel()], FlashQuestions.ASK_LIST_LENGTH);
 //		TestEquations ();
 		formatProvider = MDCulture.GetCulture();
 	}
@@ -51,9 +55,8 @@ public class FlashThrust : MonoBehaviour, OnCorrectAnswer, OnQuestionChanged {
 //		}
 
 			speed -= gravity * Time.deltaTime;
-			if (speed < 0 && noMoreQuestions) {
-				speed = 0;
-				isRunning = false;
+			if (speed < minSpeed) {
+				speed = minSpeed;
 			}
 			height += speed * Time.deltaTime;
 			if (height < 0) {
@@ -72,13 +75,18 @@ public class FlashThrust : MonoBehaviour, OnCorrectAnswer, OnQuestionChanged {
 			recordHeightText.text = recordHeight.ToString (numFormat, formatProvider) + unit;
 			apogeeText.text = apogee.ToString (numFormat, formatProvider) + unit;
 
-			if (!isRunning) {
+			if (speed <= 0 && noMoreQuestions) {
 				OnDone ();
+//				Debug.Log("Coasting time = " + (Time.time - prevTime));
 			}
 		}
 	}
 
 	public void OnCorrectAnswer(Question question, bool isNewlyMastered) {
+//		Debug.Log ("t = " + (Time.time - startTime) + " oldSpeed = " + speed + " newSpeed = " + (speed + accelerationOnCorrect) + " height = " + height);
+		if (speed < 0) {
+			speed = 0; // anything else is too discouraging.
+		}
 		speed += accelerationOnCorrect;
 		++numAnswersGiven;
 	}
@@ -86,10 +94,24 @@ public class FlashThrust : MonoBehaviour, OnCorrectAnswer, OnQuestionChanged {
 	public void OnQuestionChanged(Question question) {
 		if (question == null) {
 			noMoreQuestions = true;
+		} else { // test
+//			StartCoroutine (AutoAnswerQuestion ());
 		}
+
 	}
 
+//	IEnumerator AutoAnswerQuestion() {
+//		startTime = Time.time;
+//		while(numAnswersGiven < FlashQuestions.ASK_LIST_LENGTH) {
+//			prevTime = Time.time;
+//			OnCorrectAnswer(null, false);
+//			yield return new WaitForSeconds(targetAnswerTime);
+//		}
+//		noMoreQuestions = true;
+//	}
+
 	void OnDone() {
+		isRunning = false;
 		noMoreQuestions = false;
 		flashQuestions.wasFilled = false;
 		launch.ShowLaunchButton ();
@@ -101,9 +123,11 @@ public class FlashThrust : MonoBehaviour, OnCorrectAnswer, OnQuestionChanged {
 		apogee = 0;
 		speed = 0;
 		numAnswersGiven = 0;
+		noMoreQuestions = false;
 		celebrate.curParticleIdx = RocketParts.GetUpgradeLevel ();
 	}
 
+	// The rocket reaches speed zero at targetAnswerTime. Calculate acceleration and gravity to guarantee given maxHeight
 	void CalcParams(float maxHeight, int numChancesToAccelerate) {
 		// we want the player to reach v = 0 at targetAnswerTime (=:t) seconds after getting accelerated (1)
 		// Let v = accelerationOnCorrect
@@ -117,58 +141,72 @@ public class FlashThrust : MonoBehaviour, OnCorrectAnswer, OnQuestionChanged {
 		// (5) & (6) => H / n = v * t / 2 => 2H / nt = v
 		accelerationOnCorrect = 2 * maxHeight / (numChancesToAccelerate * targetAnswerTime);
 		gravity = accelerationOnCorrect / targetAnswerTime;
+		minSpeed = -accelerationOnCorrect * minSpeedFactor;
 	}
 
-//	float CalcAcceleration(float maxHeight, int numChancesToAccelerate) {
-//		const int n = FlashQuestions.ASK_LIST_LENGTH;
-//		float h = maxAttainableHeight;
-//		float g = gravity;
-//		float t = targetAnswerTime;
-//		float k = n * (n-1) * t / 2; // or ((n*n * t)/2 - (n * t)/2);
-//		// solve h = (n * x * (g * (t - n * t) + n * x)) / (2 * g) for x  // equation is from TestEquations()
-//		// x = -(g (sqrt((2 h n^2)/g + ((n^2 t)/2 - (n t)/2)^2) - (n^2 t)/2 + (n t)/2))/n^2
-//		//   = -(g (sqrt((2 h n^2)/g + k^2) - k))/n^2
-//		//   = -(g * (sqrt      ((2 * h * n^2)  / g + k^2)   - k))/n^2
-//		//   = -(g * (sqrt      ( 2 * h * n^2   / g + k^2)   - k))/n^2
-//		//   = -(g * (sqrt      ( 2 * h * n^2   / g + k^2)   - k))/n^2
-//		float root = Mathf.Sqrt( 2 * h * n * n / g + k * k);
-//		float accel = g * (k + root) / (n * n);
-//		if (accel < 0) {
-//			accel = g * (k - root) / (n * n);
-//		}
-//		UnityEngine.Assertions.Assert.IsTrue (accel > 0);
-//		return accel;
-//	}
-
-	void TestEquations() {
-//		accelerationOnCorrect = 2.0f * targetAnswerTime * gravity;
-		Debug.Log("Acceleration = " + accelerationOnCorrect + " gravity = " + gravity);
+	// Fixed gravity, acceleration calculated to reach maxHeight
+	// Todo: I guess what we want is to calculate gravity and acceleration based on reaching a velocity of e.g. (initialVelocity + accelerationOnCorrect/2), since slowing significantly in the time it takes to answer a question makes things look more exciting
+	float CalcAcceleration(float h, int n) {
 		float g = gravity;
 		float t = targetAnswerTime;
-		float decelarationByNextQuestion = g * t;
-		const int n = FlashQuestions.ASK_LIST_LENGTH;
-		for (int i = 0; i < n; ++i) {
-			// right after you answer the question:
-			speed += accelerationOnCorrect; 
-			// by the time you answer the next question in targetAnswerTime:
-			height += (speed - 0.5f * decelarationByNextQuestion) * t; 
-			speed -= decelarationByNextQuestion;
-			Debug.Log ("speed = " + speed + " height = " + height);
+		float k = n * (n-1) * t / 2; // or ((n*n * t)/2 - (n * t)/2);
+		// solve h = (n * x * (g * (t - n * t) + n * x)) / (2 * g) for x  // equation is from TestEquations()
+		// x = -(g (sqrt((2 h n^2)/g + ((n^2 t)/2 - (n t)/2)^2) - (n^2 t)/2 + (n t)/2))/n^2
+		//   = -(g (sqrt((2 h n^2)/g + k^2) - k))/n^2
+		//   = -(g * (sqrt      ((2 * h * n^2)  / g + k^2)   - k))/n^2
+		//   = -(g * (sqrt      ( 2 * h * n^2   / g + k^2)   - k))/n^2
+		//   = -(g * (sqrt      ( 2 * h * n^2   / g + k^2)   - k))/n^2
+		float root = Mathf.Sqrt( 2 * h * n * n / g + k * k);
+		float accel = g * (k + root) / (n * n);
+		if (accel < 0) {
+			accel = g * (k - root) / (n * n);
 		}
-		// height and speed at t = (targetAnswerTime after you answer the last question) = FlashQuestions.ASK_LIST_LENGTH * targetAnswerTime
-		float totalAnswerTime = n * t;
-		const int n1 = n + 1;
-		const int gauss	 = n1 * (n1 + 1) / 2;
-		height = decelarationByNextQuestion * totalAnswerTime * 0.5f + t * (accelerationOnCorrect - decelarationByNextQuestion) * (n1 * n1 - gauss);
-		speed = n * accelerationOnCorrect - g * totalAnswerTime; 
-		Debug.Log ("Closed form speed = " + speed + " height = " + height);
-		height += speed * speed / (2.0f * g);
-		float x = accelerationOnCorrect;
-		float altHeight = (n * x * (g * (t - n * t) + n * x)) / (2 * g);
-		float timeToStop = speed / g + totalAnswerTime;
-		Debug.Log ("final height = " + height + " or " + altHeight + " time = " + timeToStop + " of which freefall = " + speed / g);
-		height = 0;
-		speed = 0;
-	
+		UnityEngine.Assertions.Assert.IsTrue (accel > 0);
+		minSpeed = 0;
+		return accel;
 	}
+				
+//	void TestEquations() {
+//		Debug.Log("Acceleration = " + accelerationOnCorrect + " gravity = " + gravity);
+//		ShowResultsForT (0);
+//		ShowResultsForT (targetAnswerTime);
+//		ShowResultsForT (targetAnswerTime * 5);
+////		ShowResultsForT (targetAnswerTime * 10);
+//	
+//	}
+//
+//	void ShowResultsForT (float t)
+//	{
+//		float g = gravity;
+//		const int n = FlashQuestions.ASK_LIST_LENGTH;
+//		float decelarationByNextQuestion = Mathf.Min(accelerationOnCorrect, g * t);
+//		for (int i = 0; i < n; ++i) {
+//			// right after you answer the question:
+//			if (speed < 0) {
+//				speed = 0;
+//			}
+//			speed += accelerationOnCorrect;
+//			// by the time you answer the next question in targetAnswerTime:
+//			height += (speed - 0.5f * decelarationByNextQuestion) * t;
+//			speed -= decelarationByNextQuestion;
+//			if (speed < minSpeed) {
+//				speed = minSpeed;
+//			}
+//			Debug.Log ("speed = " + speed + " height = " + height);
+//		}
+//		// height and speed at t = (targetAnswerTime after you answer the last question) = FlashQuestions.ASK_LIST_LENGTH * targetAnswerTime
+//		float totalAnswerTime = n * t;
+//		const int n1 = n + 1;
+//		const int gauss	 = n1 * (n1 + 1) / 2;
+//		height = decelarationByNextQuestion * totalAnswerTime * 0.5f + t * (accelerationOnCorrect - decelarationByNextQuestion) * (n1 * n1 - gauss);
+//		speed = n * accelerationOnCorrect - g * totalAnswerTime; 
+//		Debug.Log ("Closed form speed = " + speed + " height = " + height);
+//		height += speed * speed / (2.0f * g);
+//		float x = accelerationOnCorrect;
+//		float altHeight = (n * x * (g * (t - n * t) + n * x)) / (2 * g);
+//		float timeToStop = speed / g + totalAnswerTime;
+//		Debug.Log ("final height = " + height + " or " + altHeight + " time = " + timeToStop + " of which freefall = " + speed / g);
+//		height = 0;
+//		speed = 0;
+//	}
 }
