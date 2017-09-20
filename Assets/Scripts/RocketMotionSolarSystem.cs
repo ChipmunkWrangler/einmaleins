307ff11430ba1979bf0f468e9ce31e40433ab46b
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-09-20 15:21:42 +0200

    RocketMotionSolarSystem positions the rocket relative to earth and moon.

---
+++ Assets/Scripts/RocketMotionSolarSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketMotionSolarSystem : MonoBehaviour {
+	[SerializeField] FlashThrust thrust = null;
+	[SerializeField] float heightScale = 0.0001f;
+	float minY;
+
+	void Start() {
+		minY = gameObject.transform.position.y;
+	}
+
+	void Update () {
+		Vector3 pos = gameObject.transform.position;
+
+		pos.y = minY + thrust.height * heightScale * gameObject.transform.parent.localScale.y;
+		gameObject.transform.position = pos;
+	}
+}
