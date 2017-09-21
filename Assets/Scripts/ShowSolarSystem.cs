f039e604e771c2ef7132d164f51ea3da78c53318
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-09-21 10:43:58 +0200

    Scale particles with foreground

--- Assets/Scripts/ShowSolarSystem.cs
+++ Assets/Scripts/ShowSolarSystem.cs
@@ -11,19 +11,30 @@ public class ForegroundDisplaySettings {
 public class ShowSolarSystem : MonoBehaviour {
 	[SerializeField] float verticalPadding = 0.1f; // 1.0 would be the whole screen
 	[SerializeField] Renderer rocket = null;
+	[SerializeField] GameObject particleParent = null;
+	Vector3 originalScale;
+	Transform particleSystemTransform;
+
+	void Start() {
+		originalScale = transform.localScale;
+	}
 
 	void Update() {
+		if (particleSystemTransform == null) {
+			ParticleSystem sys = particleParent.GetComponentInChildren<ParticleSystem> (false);
+			if (sys != null) {
+				particleSystemTransform = sys.transform;
+			}
+		}
+
 		float viewportTop = Camera.main.WorldToViewportPoint(rocket.bounds.max).y + verticalPadding;
 		if (viewportTop > 1.0f) {
 			transform.localScale /= viewportTop;
+			particleSystemTransform.localScale /= viewportTop;
 		}
 	}
 
-	void MoveTo (ForegroundDisplaySettings settings, float transitionTime) {
-		Transform transform = gameObject.transform;
-		transform.localScale = new Vector3 (settings.scale, settings.scale, 1.0f);
-		Vector3 oldPos = transform.position;
-		transform.position = new Vector3 (oldPos.x, settings.yPos, oldPos.z);
+	public void Reset() {
+		transform.localScale = originalScale;
 	}
-
 }
