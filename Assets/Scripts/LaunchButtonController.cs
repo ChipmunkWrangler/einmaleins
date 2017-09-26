489cf0bc74b9ce997a1f53e439dbc8b5fad39dd7
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-09-26 16:04:26 +0200

    Show different launch button texts depending on next target planet.

--- Assets/Scripts/LaunchButtonController.cs
+++ Assets/Scripts/LaunchButtonController.cs
@@ -4,10 +4,20 @@ using UnityEngine.UI;
 public class LaunchButtonController : MonoBehaviour, OnQuestionChanged {
 	[SerializeField] Button launchButton = null;
 	[SerializeField] Button upgradeButton = null;
+	[SerializeField] Text launchButtonText = null;
 	[SerializeField] Text upgradeButtonLabel = null;
 	[SerializeField] Text doneText = null;
 	[SerializeField] string buildRocketText = "";
 	[SerializeField] string upgradeRocketText = "";
+	readonly string[] launchButtonLabels = {
+		"Auf zum Mars",
+		"Auf zur Venus",
+		"Auf zum Jupiter",
+		"Auf zum Saturn",
+		"Auf zum Uranus",
+		"Auf zum Neptun",
+		""
+	};
 
 	public void OnQuestionChanged(Question question) {
 		bool noMoreQuestions = question == null;
@@ -21,6 +31,7 @@ public class LaunchButtonController : MonoBehaviour, OnQuestionChanged {
 		bool canUpgrade = RocketParts.CanUpgrade ();
 		upgradeButtonLabel.text = canBuild ? buildRocketText : upgradeRocketText;
 		upgradeButton.gameObject.SetActive (noMoreQuestions && (canBuild || canUpgrade));
+		launchButtonText.text = launchButtonLabels [TargetPlanet.GetIdx ()];
 		launchButton.gameObject.SetActive (noMoreQuestions && canLaunch);
 		doneText.gameObject.SetActive( noMoreQuestions && !canLaunch && !canUpgrade && !canBuild);
 
