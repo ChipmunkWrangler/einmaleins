bb24f0d07d688b26ed0cd292912b0cd7b2484e8e
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-10-13 11:41:25 +0200

    Particle effect in main scene are resolution-independent.

--- Assets/Scripts/PositionFx.cs
+++ Assets/Scripts/PositionFx.cs
@@ -5,17 +5,9 @@ using UnityEngine;
 public class PositionFx : MonoBehaviour {
 	[SerializeField] Transform referencePoint;
 
-	bool isSet;
-	// Use this for initialization
 	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (!isSet) { 
-			isSet = true;
-//			print (transform.position);
-//			gameObject.transform.position = Camera.main.WorldToScreenPoint(transform.position);
-		}
+		Vector3 newPos = Camera.main.ScreenToWorldPoint(referencePoint.position);
+		newPos.z = gameObject.transform.position.z;
+		gameObject.transform.position = newPos;
 	}
 }
