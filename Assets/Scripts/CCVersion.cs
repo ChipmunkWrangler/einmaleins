45d822732dad7825ffe48d4e983978d73f7bd727
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-11-14 17:57:48 +0100

    Add version code to prefs. At the moment, just delete old prefs and restart if the versions don't match.

---
+++ Assets/Scripts/CCVersion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCVersion : MonoBehaviour {
+	static int majorVersion = 0;
+	static int minorVersion = 2;
+	static int buildNumber = 0;
+
+	static public string GetCurrentVersion() {
+		return majorVersion + "." + minorVersion + "." + buildNumber;
+	}
+		
+	public void CheckVersion() {
+		if (IsVersionMismatched()) {
+			print ("Mismatch");
+			RestartWithNewVersion ();
+			StartCoroutine(RestartWithNewVersion ());
+		}
+	}
+
+	void OnApplicationPause(bool pauseStatus) {
+		if (!pauseStatus) {
+			print ("Unpaused");
+			CheckVersion ();
+		}
+	}
+
+	bool IsVersionMismatched() {
+		return PlayerPrefs.GetString ("version") != GetCurrentVersion ();
+	}
+
+	IEnumerator RestartWithNewVersion() {
+		print ("Restart");
+		yield return new WaitForEndOfFrame ();
+		print ("2");
+		PlayerPrefs.DeleteAll ();
+		PlayerPrefs.SetString ("version", GetCurrentVersion ());
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("choosePlayer");
+		print("4");
+	}
+}
---
+++ Assets/Scripts/VersionDisplay.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VersionDisplay : MonoBehaviour {
+	[SerializeField] UnityEngine.UI.Text text;
+	void Start () {
+		text.text = CCVersion.GetCurrentVersion ();
+	}
+}
