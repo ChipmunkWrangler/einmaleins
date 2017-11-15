aa8baf04e60e8ece5f2a96798266e6769e1d51c0
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-11-15 08:35:08 +0100

    [BUGFIX] Called restart twice. Updated build number because of upload error

--- Assets/Scripts/CCVersion.cs
+++ Assets/Scripts/CCVersion.cs
@@ -3,40 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CCVersion : MonoBehaviour {
-	static int majorVersion = 0;
-	static int minorVersion = 2;
-	static int buildNumber = 0;
+	const int majorVersion = 0;
+	const int minorVersion = 1;
+	const int buildNumber = 10;
 
 	static public string GetCurrentVersion() {
 		return majorVersion + "." + minorVersion + "." + buildNumber;
 	}
 		
 	public void CheckVersion() {
-		if (IsVersionMismatched()) {
-			print ("Mismatch");
-			RestartWithNewVersion ();
+		string oldVersion = PlayerPrefs.GetString ("version");
+		if (oldVersion == GetCurrentVersion ()) {
+			return;
+		}
+		PlayerPrefs.SetString ("version", GetCurrentVersion ());
+		switch( oldVersion ) {
+		case "0.1.9":
+		case "0.1.8":
+			break;
+		default:
 			StartCoroutine(RestartWithNewVersion ());
+			break;
 		}
 	}
 
 	void OnApplicationPause(bool pauseStatus) {
 		if (!pauseStatus) {
-			print ("Unpaused");
 			CheckVersion ();
 		}
 	}
 
-	bool IsVersionMismatched() {
-		return PlayerPrefs.GetString ("version") != GetCurrentVersion ();
-	}
-
 	IEnumerator RestartWithNewVersion() {
-		print ("Restart");
 		yield return new WaitForEndOfFrame ();
-		print ("2");
 		PlayerPrefs.DeleteAll ();
-		PlayerPrefs.SetString ("version", GetCurrentVersion ());
 		UnityEngine.SceneManagement.SceneManager.LoadScene ("choosePlayer");
-		print("4");
 	}
 }
