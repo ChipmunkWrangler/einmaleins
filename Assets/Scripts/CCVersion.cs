35e558a8175e0d8f85c170d0f3899b1e9efde7ec
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-11-15 12:49:11 +0100

    CCVersion now loads its own scene before messing with player prefs.

--- Assets/Scripts/CCVersion.cs
+++ Assets/Scripts/CCVersion.cs
@@ -1,34 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class CCVersion : MonoBehaviour {
 	const int majorVersion = 0;
 	const int minorVersion = 1;
 	const int buildNumber = 10;
 
+	bool isChecking;
+
 	static public string GetCurrentVersion() {
 		return majorVersion + "." + minorVersion + "." + buildNumber;
 	}
 		
 	public void CheckVersion() {
+		if (isChecking) {
+			return;
+		}
+		isChecking = true;
 		string oldVersion = PlayerPrefs.GetString ("version");
 		if (oldVersion == GetCurrentVersion ()) {
 			return;
 		}
-		PlayerPrefs.SetString ("version", GetCurrentVersion ());
 		switch( oldVersion ) {
 		case "0.1.9":
 		case "0.1.8":
-			break;
-		case "0.1.7":
-			StartCoroutine (Update_0_1_7 ());
+			WriteNewVersion ();
 			break;
 		default:
-			StartCoroutine(RestartWithNewVersion ());
+			RestartWithNewVersion ();
 			break;
 		}
+	}
+
+	void Start() {
+		CheckVersion();
 	}
 
 	void OnApplicationPause(bool pauseStatus) {
@@ -37,55 +44,19 @@ public class CCVersion : MonoBehaviour {
 		}
 	}
 
-	IEnumerator RestartWithNewVersion() {
-		//if scene is update, do update
-		// otherwise, goto update
-		UnityEngine.SceneManagement.SceneManager.LoadScene ("choosePlayer");
-		yield return new WaitForEndOfFrame ();
-		yield return new WaitForEndOfFrame ();
-		PlayerPrefs.DeleteAll ();
-		TODO choosePlazer should check Version first
-	}
-
-	IEnumerator Update_0_1_7() {
-		yield return new WaitForEndOfFrame ();
-		string log = "Start";
-		string[] playerNames = NewPlayerName.GetPlayerNames ();
-		foreach (string playerName in playerNames) {
-			log += "\nUpdating player " + playerName;
-			log += NewPlayerName.SetCurPlayerName (playerName);
-			Update_0_1_7_Player ();
-		}
-		log += "\nEnd";
-		UnityEngine.SceneManagement.SceneManager.LoadScene ("choosePlayer");
-	}
-
-	string Update_0_1_7_Player() {
-		if (!Questions.WereQuestionsCreated ()) {
-			return;
-		}
-		string log;
-		Question[] questions = Questions.CreateQuestions ();
-		for (int i = 0; i < questions.Length; ++i) {
-			log += Update_0_1_7_Question(Questions.GetQuestionKey(i), i);
-		}
-		log += Update_0_1_7_RocketParts ();
-		return log;
-	}
-
-	string Update_0_1_7_Question(string prefsKey, int questionIdx) {
-		List<float> answerTimes = Question.GetAnswerTimes(prefsKey);
-		bool isNew = answerTimes.All (answerTime => Mathf.Approximately (answerTime, 60f));
-		MDPrefs.SetBool (prefsKey + ":isNew", isNew);
-		string log = "\nQuestion " + questionIdx + " isNew = " + isNew;
-		if (isNew) {
-			Question.SetAnswerTimes (prefsKey, Question.GetNewAnswerTimes ());
+	void RestartWithNewVersion() {
+		if (SceneManager.GetActiveScene ().name == "updateVersion") {
+			PlayerPrefs.DeleteAll ();
+			WriteNewVersion ();
+			SceneManager.LoadScene ("choosePlayer");
+		} else {
+			SceneManager.LoadScene ("updateVersion");
 		}
 	}
 
-	string Update_0_1_7_RocketParts() {
-		RocketParts rocketParts = RocketParts.instance;
-		rocketParts.isRocketBuilt = true;
-		//		rocketParts.upgradeLevel = 
+	void WriteNewVersion() {
+		PlayerPrefs.SetString ("version", GetCurrentVersion ());
+		PlayerPrefs.Save ();
+		isChecking = false;
 	}
 }
