0d19e7591e8b456e490fc1104d2d3659961c03c9
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-11-17 12:30:07 +0100

    Handle Android back button

---
+++ Assets/Scripts/KeyHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyHandler : MonoBehaviour {
+	[SerializeField] string backSceneName = "choosePlayer";
+	bool isLoadingScene;
+
+	void Update () {
+		if (!isLoadingScene && Input.GetKeyDown(KeyCode.Escape)) { 
+			isLoadingScene = true;
+			if (backSceneName == "EXIT") {
+				Application.Quit ();
+			} else {
+				UnityEngine.SceneManagement.SceneManager.LoadSceneAsync (backSceneName);
+			}
+		}  	
+	}
+}
