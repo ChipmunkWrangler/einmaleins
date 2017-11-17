14b3b4594561c919ba92fc683a52075199634421
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-11-17 09:49:12 +0100

    Moved I2 plugin back to default position.

--- Assets/Scripts/MDCulture.cs
+++ Assets/Scripts/MDCulture.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Linq;
+﻿using System.Linq;
 
 public class MDCulture {
 	static System.Globalization.CultureInfo ci;
