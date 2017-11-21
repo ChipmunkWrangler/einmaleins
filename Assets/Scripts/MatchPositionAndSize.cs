78c64929fb50408c11b64a81b30d73d952de0b8e
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-11-21 16:21:11 +0100

    Question button appears at correct position and size in grid layout.

---
+++ Assets/Scripts/MatchPositionAndSize.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchPositionAndSize : MonoBehaviour {
+	[SerializeField] RectTransform target;
+	[SerializeField] RectTransform source;
+
+	void Update () {
+		source.position = target.position;
+		source.sizeDelta = target.sizeDelta;
+	}
+}
