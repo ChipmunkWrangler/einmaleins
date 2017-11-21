c171f7eac496e2ed94ed5cc9b5395ff03a81701e
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-11-21 15:44:36 +0100

    Add question mark "give up" button (nonfunctional so far).

--- Assets/Scripts/InactiveOnAnswer.cs
+++ Assets/Scripts/InactiveOnAnswer.cs
@@ -2,18 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InactiveOnAnswer : MonoBehaviour, OnCorrectAnswer, OnWrongAnswer, OnAnswerChanged {
+public class InactiveOnAnswer : MonoBehaviour, OnCorrectAnswer, OnWrongAnswer, OnAnswerChanged, OnQuizAborted, OnQuestionChanged {
 	[SerializeField] UnityEngine.UI.Button button = null;
+	[SerializeField] bool showOnEmptyAnswer;
+	[SerializeField] float transitionTime = 0.5f;
+
+	public void OnQuizAborted() {
+		SetInteractibility (false);
+	}
 
 	public void OnCorrectAnswer (Question question, bool isNewlyMastered) {
-		button.interactable = false;
+		SetInteractibility (false);
 	}
 
 	public void OnWrongAnswer (bool wasNew) {
-		button.interactable = false;
+		SetInteractibility (showOnEmptyAnswer);
+	}
+
+	public void OnQuestionChanged(Question question) {
+		SetInteractibility (question != null && showOnEmptyAnswer);
 	}
 
 	public void OnAnswerChanged(bool isAnswerEmpty) {
-		button.interactable = !isAnswerEmpty;
+		SetInteractibility( isAnswerEmpty == showOnEmptyAnswer );
+	}
+		
+	void SetInteractibility(bool b) {
+		if (button.interactable != b) {
+			button.interactable = b;
+			ScaleTo (b ? Vector3.one : Vector3.zero);
+		}
+	}
+
+	void ScaleTo(Vector3 tgtScale) {
+		iTween.ScaleTo(gameObject, iTween.Hash( "scale", tgtScale, "easeType", iTween.EaseType.easeInSine, "time", transitionTime));
 	}
 }
