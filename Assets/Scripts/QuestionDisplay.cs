0dc28c2d0bf45b0236796c86d577a54d2279d4fc
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-11-22 22:41:10 +0100

    [BUGFIX] English questions show cross instead of dot for multiplication.

--- Assets/Scripts/QuestionDisplay.cs
+++ Assets/Scripts/QuestionDisplay.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using I2;
 
 public class QuestionDisplay : TextDisplay, OnQuestionChanged, OnQuizAborted, OnCorrectAnswer {
 	public void OnQuestionChanged(Question question) {
 		string s = "";
 		if (question != null) {			
 			int x = Random.Range (0, 2);
-			s = (x == 0) ? question.a + " · " + question.b : question.b + " · " + question.a;
+			s = (x == 0) ? question.a + I2.Loc.LocalizationManager.GetTermTranslation( "multiplicationDot" ) + question.b : question.b + " · " + question.a;
 			s += " = ";
 		}
 		SetText(s);
