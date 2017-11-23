c442134f14b4264ef5cda27de6b18002d2918c59
Rafael Van Daele-Hunt <rafael@flaregames.com>
2017-11-23 15:40:02 +0100

    Colour wheel now reacts if you leave your finger down.

--- Assets/Scripts/ColorPickerCircle.cs
+++ Assets/Scripts/ColorPickerCircle.cs
@@ -22,7 +22,7 @@ public class ColorPickerCircle : MonoBehaviour {
     }
 	
 	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
+		if (Input.GetMouseButton (0)) {
 			CurLocalPos = GetIntersection ();
             CheckCirclePosition();
         }
