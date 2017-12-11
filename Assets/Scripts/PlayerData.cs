1dec6b639c2dce1b8f08caf7d9e906d83c524a25
Rafael <rafael@Rafaels-MacBook-Air.local>
2017-12-11 16:59:07 +0100

    Dump stats screen as well.

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -3,12 +3,14 @@ public class PlayerData {
 	public string playerName;
 	public RocketPartsPersistantData rocketPartsData = new RocketPartsPersistantData ();
 	public EffortTrackerPersistantData effortTrackerData = new EffortTrackerPersistantData ();
-	public QuestionsPersistentData questions = new QuestionsPersistentData();
+	public QuestionsPersistentData questionsData = new QuestionsPersistentData();
+	public StatsControllerPersistentData statsControllerData = new StatsControllerPersistentData();
 
 	public void Load(string name) {
 		playerName = name;
 		rocketPartsData.Load ();
 		effortTrackerData.Load ();
-		questions.Load ();
+		questionsData.Load ();
+		statsControllerData.Load (Questions.maxNum);
 	}
 }
