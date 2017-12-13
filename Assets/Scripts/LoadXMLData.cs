40292d9e62cf479331222914f7ccbc318f06cd3d
Rafael <rafael@Rafaels-MacBook-Air.local>
2017-12-13 18:06:54 +0100

    [REFACTOR] autoformat

--- Assets/Scripts/LoadXMLData.cs
+++ Assets/Scripts/LoadXMLData.cs
@@ -3,26 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LoadXMLData : MonoBehaviour
-{
+public class LoadXMLData : MonoBehaviour {
 	[SerializeField] Text inputField;
 	[SerializeField] UnityEngine.UI.Text statusLine = null;
 
-	public void LoadFromInputField ()
-	{
+	public void LoadFromInputField () {
 		try {
-			XMLSerializationHandler.LoadFromString (inputField.text);
+			XMLSerializationHandler.LoadFromString( inputField.text );
 		} catch (System.Exception ex) {
-			statusLine.text = AssemblyCSharp.ExceptionPrettyPrint.Msg (ex);
+			statusLine.text = AssemblyCSharp.ExceptionPrettyPrint.Msg( ex );
 		}	
 	}
 
-	public void LoadFromFile ()
-	{
+	public void LoadFromFile () {
 		try {
-			XMLSerializationHandler.LoadFromFile ();
+			XMLSerializationHandler.LoadFromFile();
 		} catch (System.Exception ex) {
-			statusLine.text = AssemblyCSharp.ExceptionPrettyPrint.Msg (ex);
+			statusLine.text = AssemblyCSharp.ExceptionPrettyPrint.Msg( ex );
 		}	
 		statusLine.text = I2.Loc.LocalizationManager.GetTermTranslation ("Data loaded.");
 	}
