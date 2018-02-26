714ad9ba40bc1b5f55b32c211520977f963537a2
Rafael <>
2018-02-26 13:56:36 +0100

    [REFACTOR] Removed some unused using directives

--- Assets/Scripts/GiveUpButtonController.cs
+++ Assets/Scripts/GiveUpButtonController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 class GiveUpButtonController : MonoBehaviour, IOnWrongAnswer, IOnQuizAborted, IOnQuestionChanged
--- Assets/Scripts/IOnQuestionChanged.cs
+++ Assets/Scripts/IOnQuestionChanged.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-internal interface IOnQuestionChanged
+﻿internal interface IOnQuestionChanged
 {
     void OnQuestionChanged(Question newQuestion);
 }
