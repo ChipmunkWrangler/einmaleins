c25e8dde249fa15b0ef1b43fe46f11f23bcd47f9
Rafael <>
2018-02-28 23:34:33 +0100

    Added editor code to test events.

---
+++ Assets/Scripts/CrazyChipmunk/Editor/GameEventEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace CrazyChipmunk
+{
+    [CustomEditor(typeof(GameEvent))]
+    public class GameEventEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GUI.enabled = Application.isPlaying;
+
+            GameEvent e = target as GameEvent;
+            if (GUILayout.Button("Test"))
+                e.Notify();
+        }
+    }
+}
