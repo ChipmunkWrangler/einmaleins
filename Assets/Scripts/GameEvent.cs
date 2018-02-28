d3f3a119e09ca875f76a6494c2b8ea16d5a2acdf
Rafael <>
2018-02-28 22:42:25 +0100

    Add GameEvent and GameEventSubscriber classes.

---
+++ Assets/Scripts/GameEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+public class GameEvent : ScriptableObject
+{
+    List<GameEventSubscriber> subscribers = new List<GameEventSubscriber>();
+
+    public void Notify()
+    {
+        for (int i = subscribers.Count; i >= 0; --i)
+        {
+            subscribers[i].OnGameEvent();
+        }
+    }
+
+    public void Subscribe(GameEventSubscriber subscriber)
+    {
+        Assert.IsFalse(subscribers.Contains(subscriber), "Duplicate subscriber to " + name + ": " + subscriber.name );
+        subscribers.Add(subscriber);
+    }
+
+    public void Unsubscribe(GameEventSubscriber subscriber)
+    {
+        subscribers.Remove(subscriber);
+    }
+}
---
+++ Assets/Scripts/GameEventSubscriber.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventSubscriber : MonoBehaviour {
+    [SerializeField] GameEvent subscribeTo = null;
+    [SerializeField] UnityEvent respondWith = null;
+
+    public void OnGameEvent() {
+        respondWith.Invoke();
+    }
+
+    void OnEnable() {
+        subscribeTo.Subscribe(this);
+    }
+    private void OnDisable()
+    {
+        subscribeTo.Unsubscribe(this);
+    }
+}
