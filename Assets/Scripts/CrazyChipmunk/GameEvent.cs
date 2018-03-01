606b08de0315d534ee141d675a9aca55468ff3af
Rafael <>
2018-03-01 10:55:10 +0100

    Added logging to GameEvent and always prevent duplicate subscribers.

--- Assets/Scripts/CrazyChipmunk/GameEvent.cs
+++ Assets/Scripts/CrazyChipmunk/GameEvent.cs
@@ -13,18 +13,27 @@ namespace CrazyChipmunk
         {
             for (int i = subscribers.Count - 1; i >= 0; --i)
             {
+                Debug.Log("Sending " + name + " to " + subscribers[i].name);
                 subscribers[i].OnEvent();
             }
         }
 
         public void Subscribe(GameEventSubscriber subscriber)
         {
-            Assert.IsFalse(subscribers.Contains(subscriber), "Duplicate subscriber to " + name + ": " + subscriber.name);
-            subscribers.Add(subscriber);
+            if (subscribers.Contains(subscriber))
+            {
+                Debug.LogError("Duplicate subscriber to " + name + ": " + subscriber.name);
+            }
+            else
+            {
+                Debug.Log("Subscribing " + subscriber.name + " to " + name);
+                subscribers.Add(subscriber);
+            }
         }
 
         public void Unsubscribe(GameEventSubscriber subscriber)
         {
+            Debug.Log("Unsubscribing " + subscriber.name + " from " + name);
             subscribers.Remove(subscriber);
         }
     }
